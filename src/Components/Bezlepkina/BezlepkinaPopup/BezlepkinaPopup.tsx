6737730a14866136c513064b4c3fa4050536a9cb
MishaFil <fme300605@yandex.ru>
2023-04-06 11:37:45 +0700

    создание модального окна

---
+++ src/Components/Bezlepkina/BezlepkinaPopup/BezlepkinaPopup.tsx
@@ -0,0 +1,37 @@
+import { Button, Typography } from '@mui/material';
+import { Modal } from '@mui/material';
+import React from 'react';
+import "./BezlepkinaPopup.scss"
+
+type Props = {
+    open: boolean,
+    onClose: () => void;
+    children: any,
+    title: string
+}
+const BezlepkinaPopup = ({ open, onClose, children, title }: Props) => {
+    return (
+      < Modal
+    open = { open }
+    onClose = {()=> onClose() }
+        >
+            <Typography className='popup_bezlepkina'>
+                <div className='popup_bezlepkina_content'>
+                    <div className='popup_bezlepkina_content_header'>
+                        <div>
+                            {title}
+                        </div>
+                        <div>
+                            <Button onClick={() => onClose()}>
+                                close
+                            </Button>
+                        </div>
+                    </div>
+                {children}
+                </div>
+            </Typography>
+        </Modal>
+    )
+}
+
+export default BezlepkinaPopup;
--- src/Pages/StudentPage/Bezlepkina/Category/Category.tsx
+++ src/Pages/StudentPage/Bezlepkina/Category/Category.tsx
@@ -1,6 +1,13 @@
 
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton/IconButton';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import React, { useState } from 'react';
-import {Category} from './models'
+import { Category } from './models'
+import EditIcon from '@mui/icons-material/Edit';
+import SendIcon from '@mui/icons-material/Send';
+import BezlepkinaPopup from '../../../../Components/Bezlepkina/BezlepkinaPopup/BezlepkinaPopup';
+import { Button } from '@mui/material';
 
 const BezlepkinaCategory = () => {
 
@@ -15,14 +22,70 @@ const BezlepkinaCategory = () => {
         },
     ])
 
+    const onDeleteClick = (id: number) => {
+        setcategoryList(prev =>
+            prev.filter(el => el.id !== id))
+    }
+
+    const columns: GridColDef[] = [{
+            field: 'id',
+            headerName: 'Id'
+        },
+        {
+            field: 'name',
+            headerName: 'Name'
+        },
+        {
+            field: '',
+            headerName: '',
+            renderCell: (e: any) => {
+                return <div style={{ display: 'flex', gap: '1em' }}>
+                    <IconButton aria-label="delete"
+                        onClick={() => onDeleteClick(e.row.id)}>
+                        <DeleteIcon />
+                    </IconButton>
+
+                    <IconButton aria-label="edit">
+                        <EditIcon />
+                    </IconButton>
+                </div>
+            }
+        }
+    ]
+
     return (
-        <div>
-            <h1>Category </h1>
+        <div style={{ width: '100%' }}>
+            <BezlepkinaPopup title={'category create'} open={true} onClose={() => { }}>
+                </BezlepkinaPopup>
+            <div style={{display:'flex',
+                justifyContent:'space-between',
+                alignItems:'center'
+            }}>
+                 <h1>Category </h1>
+            </div>
+            
             <div>
-                {
-                    categoryList.map((el, i) => <div>{el.name}</div>)
-                }
-         </div>
+                <Button variant="contained">
+                    Add
+                    </Button>
+          </div>
+            <div style={{ height: '80vh', width: '100%' }}>
+                <DataGrid
+                    rows={categoryList}
+                    columns={columns}
+                    initialState={{
+                        pagination: {
+                            paginationModel: {
+                                pageSize: 5,
+                            },
+                        },
+                    }}
+                    pageSizeOptions={[5]}
+                    checkboxSelection
+                    disableRowSelectionOnClick
+                />
+         
+        </div>
         </div>
     );
 };
