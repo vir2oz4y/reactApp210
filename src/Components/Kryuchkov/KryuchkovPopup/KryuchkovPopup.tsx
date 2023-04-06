5854993628cf8f129ebd4496e8e49ceba0ba3865
nicklson <nicklson1@yandex.ru>
2023-04-06 11:37:35 +0700

    создали модальное окно

---
+++ src/Components/Kryuchkov/KryuchkovPopup/KryuchkovPopup.tsx
@@ -0,0 +1,46 @@
+import { Button, Modal, Typography } from '@mui/material';
+import React from 'react';
+import "./KryuchkovPopup.scss";
+
+type Props = {
+    open: boolean,
+    onClose: () => void;
+    children: any,
+    title: string
+
+}
+
+const KryuchkovPopup = ({ open, onClose, children, title}: Props) => {
+
+
+    return (<Modal
+        open={open}
+        onClose={()=>onClose()}
+    >
+        <Typography className='kryuchkov_popup'>
+            <div className='kryuchkov_popup__content'>
+                <div className='kryuchkov_popup__content__header'>
+                    <div>
+                        {title}
+                    </div>
+
+                    <div>
+                        <Button
+                            onClick={() => onClose()}
+                        >
+                            Close
+                        </Button>
+                    </div>
+                </div>
+
+
+                <div>
+                    {children}
+                </div>
+                
+            </div>
+        </Typography>
+    </Modal>)
+}
+
+export default KryuchkovPopup;
--- src/Pages/StudentPage/KryuchkovNick/Category/CategoryPage.tsx
+++ src/Pages/StudentPage/KryuchkovNick/Category/CategoryPage.tsx
@@ -1,7 +1,12 @@
+import { Button, IconButton } from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import React, { useState } from 'react';
 import { Category } from './models';
 //import Category from "./models";
-
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import KryuchkovNickPage from '../KryuchkovNickPage';
+import KryuchkovPopup from '../../../../Components/Kryuchkov/KryuchkovPopup/KryuchkovPopup';
 
 const CategoryPage = () => {
 
@@ -17,15 +22,93 @@ const CategoryPage = () => {
     ])
 
 
+    const onDeleteClick = (id:number) => {
+        setCategoryList(prev =>
+            prev.filter(el => el.id !== id)
+        )
+    }
+
+    const columns: GridColDef[] = [
+        {
+            field: 'id',
+            headerName:'Id'
+        },
+        {
+            field: 'name',
+            headerName:'Name'
+        },
+        {
+            field: '',
+            headerName: '',
+            renderCell: (e:any) => {
+                return <div style={{display:'flex', gap:'1em'}}>
+
+                    <IconButton aria-label="edit">
+                        <EditIcon />
+                    </IconButton>
+
+                    <IconButton
+                        onClick={() => onDeleteClick(e.row.id)}
+                        aria-label="delete"
+                    >
+                        <DeleteIcon />
+                    </IconButton>
+                </div>
+            }
+        }
+    ]
+
+
 
     return (
-        <div>
-            <h1>Category</h1>
+        <div style={{ width: '100%' }}>
 
-            <div>
-                {
-                    categoryList.map((el, i) => <div>{el.name}</div>)
-                }
+            <KryuchkovPopup
+                title={'Category create'}
+                open={true}
+                onClose={() => { }}
+            >
+                <div>
+                    123412412
+                </div>
+            </KryuchkovPopup>
+
+
+            <div style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center'
+            }}>
+
+                <h1>Category</h1>
+
+                <div>
+                    <Button
+                       color={'primary'}
+                       variant={'contained'}
+                    >
+                        Add Category
+                    </Button>
+
+                </div>
+            </div>
+            
+
+            <div style={{height:'80vh', width:'100%'} }>
+                <DataGrid
+                    rows={categoryList}
+                    columns={columns}
+                    initialState={{
+                        pagination: {
+                            paginationModel: {
+                                pageSize: 5,
+                            },
+                        },
+                    }}
+                    pageSizeOptions={[5]}
+                    //checkboxSelection
+                    disableRowSelectionOnClick
+                />
             </div>
         </div>
     );
