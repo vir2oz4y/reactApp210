407e2a2f52d2526522908bf7d9a563853d46566c
Simakov <nogla99@inbox.ru>
2023-05-14 23:20:41 +0700

    added pages

--- src/Pages/StudentPage/SimakovMatvey/Manufacturer/Manufacturer.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const Manufacturer = () => <div>Manufacturer page</div>
-
-export default Manufacturer
--- src/Pages/StudentPage/SimakovMatvey/SimakovMatveyPage.tsx
+++ src/Pages/StudentPage/SimakovMatvey/SimakovMatveyPage.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../../../Components/Header/Header'
 import ContentBlock from '../../../Components/ContentBlock/ContentBlock'
 import AsideMenu from './AsideMenu/AsideMenu'
 import { Outlet } from 'react-router-dom'
 import './SimakovMatveyPage.scss'
+import axios from 'axios'
+
+export const simakovAxios = axios.create({})
+simakovAxios.defaults.headers.common['Authorization'] = localStorage.getItem('canstudyAuthToken') || ''
 
 const SimakovMatveyPage = () => {
+	const [authToken, setAuthToken] = useState('')
+
+	const doLogin = () => {
+		axios
+			.post<{ authToken: string }>('https://canstudy.ru/orderapi/user/login', {
+				identifier: '69E4531D-36EF-4A7C-AC30-CF9838B2652F',
+			})
+			.then((res) => {
+				let bearer = 'Bearer ' + res.data.authToken
+				localStorage.setItem('canstudyAuthToken', bearer)
+				simakovAxios.defaults.headers.common['Authorization'] = bearer
+			})
+	}
+
+	useEffect(() => {
+		doLogin()
+	}, [])
+
 	return (
 		<div>
 			<Header studentFio={'Симаков Матвей'} />
